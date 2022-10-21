c8469c5c238c64bf2fea744a68f4526c1c94d8ca
DenisConsta <denisconstantin338@gmail.com>
2022-10-21 13:10:51 +0200

    [add] generateBombs, reset, endGame, seeBombs

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
+//? HTML elements
 const grid = document.querySelector('.grid');
 const btn = document.querySelector('.btn');
+const output = document.querySelector('output');
+//? Arrow functions
 const dimRow = (n) => Math.sqrt(n);
-let isGenerate = false, limit;
-let list = [];
-//? listener button GENERA
-btn.addEventListener('click', function(){
+const myRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const isWin = () => score === (list.length - BOMBS);
+//? Variables
+let isGenerate = false, limit, score = 0;
+let list = [], bombsList = [];
+const BOMBS = 16;
 
+//? listener button GENERA
+btn.addEventListener('click', play);
+
+//? Funzione di gioco (play)
+function play(){
   if(!isGenerate){
     grid.classList.remove('d-none');
     isGenerate = true;
   }else{
-    grid.replaceChildren();
+    reset();
   }
-
   initGrid();
-
-});
+  bombsList = generateBombs(limit);
+  console.log(bombsList);
+}
 
 // ? Inizializza la griglia
 function initGrid(){
@@ -41,10 +51,60 @@ function createCell(index){
   return cell;
 }
 
+//? Funzione per il click della cella
 function handlerClickCell(){
   if(!this.isClicked){
     this.classList.add('clicked');
     this.isClicked = true;
     console.log(this.idCell,this.isClicked);
+
+    if(bombsList.includes(this.idCell))
+      endGame(false);
+    else
+      score++;
   }
+  
+  if(isWin())
+    endGame(true);
+}
+
+//? Genera un'array di indici dove sono presenti le bombe
+function generateBombs(cells){
+  const bombsList = [];
+  let random;
+
+  while(bombsList.length < BOMBS){
+    random = myRandom(1, cells);
+    if(!bombsList.includes(random))
+      bombsList.push(random);
+  }
+  return bombsList;
+}
+
+//? Reset campi
+function reset(){
+  grid.replaceChildren();
+  output.innerText = "";
+  score = 0;
+  list = [];
+}
+
+//? Funzione di fine gioco che lancia l'output del risultato
+function endGame(outcome){
+  if(outcome){
+    output.innerText = `HAI VINTO !`;
+  }else{
+    seeBombs();
+    output.innerText = `
+    HAI PERSO :(
+    score : ${score} su ${list.length - BOMBS}
+    `;
+  }
+}
+
+// ? Mostra tute le bombe sulla mappa
+function seeBombs(){
+  bombsList.forEach(el => {
+    list[el-1].classList.add('bomb');
+  });
 }
